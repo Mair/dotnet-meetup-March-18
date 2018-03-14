596fd817e239887ad14292631d6675583b172558
Mair Swartz <mair.swartz@essentialenergy.com.au>
2018-03-14 15:34:52 +1100

    using state to update components

--- index.tsx
+++ index.tsx
@@ -8,16 +8,22 @@ class ChildClassComp extends React.Component<propsDefinition> {
       <div>
         <input type="text" onChange={e => this.props.callBack(e.target.value)} />
       </div>
-    )
+    );
   }
 }
+interface ClassCompProps {}
+interface ClassCompState {
+  message: string;
+}
 
-class ClassComp extends React.Component {
+class ClassComp extends React.Component<ClassCompProps, ClassCompState> {
+  state ={message: ''}
   render() {
     return (
       <div>
         hello world from parent class
-        <ChildClassComp callBack={message => console.log(message)} />
+        <ChildClassComp callBack={message => this.setState({message})} />
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
