c58a51b69aedd7310b7b275e646a1c756da80104
Mair Swartz <mair.swartz@essentialenergy.com.au>
2018-03-14 15:33:52 +1100

    nested components & passing props

--- index.tsx
+++ index.tsx
@@ -1,6 +1,18 @@
+class ChildClassComp extends React.Component {
+  render(){
+    return <div>{this.props.message}</div>
+  }
+}
+
+const ChildFuncComp = props => (<div>{props.message}</div>)
+
 class ClassComp extends React.Component {
   render(){
-    return <div>hello world from class</div>
+    return <div>
+      hello world from parent class
+      <ChildFuncComp message="message sent to functional component" />
+      <ChildClassComp message="message sent to class component" />
+    </div>
   }
 }
 
